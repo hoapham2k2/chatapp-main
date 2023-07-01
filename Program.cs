2d0a48eb34d36dce7e61161c1983f0b7899a574a
hoapham2k2 <20520995@gm.uit.edu.vn>
2023-07-01 10:00:45 +0700

    update cors

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
     {
         builder.AllowAnyMethod()
             .AllowAnyHeader()
-            .WithOrigins("http://localhost:3000")
+            .WithOrigins("https://chatapp-main-rouge.vercel.app/")
             .AllowCredentials();
     });
 });
