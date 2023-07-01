e36ab0e0a34f1abf4a84887be480248639966281
hoapham2k2 <20520995@gm.uit.edu.vn>
2023-07-01 13:33:47 +0700

    update backend|

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -4,6 +4,26 @@ namespace mysignalR.Hubs
 {
     public class ChatHub : Hub
     {
+        //lây danh sách các connectionId đang kết nối
+
+        public static ICollection<string> ListConnectionId { get; set; } = new List<string>();
+
+        public ChatHub() { }
+
+        //gọi khi có 1 client kết nối tới
+        public override async Task OnConnectedAsync()
+        {
+            ListConnectionId.Add(Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
+        //gọi khi có 1 client ngắt kết nối
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            ListConnectionId.Remove(Context.ConnectionId);
+            await base.OnDisconnectedAsync(exception);
+        }
+
         public async Task SendMessage(Message message)
         {
             await Clients.All.SendAsync("ReceiveMessage", message);
--- Models/Message.cs
+++ Models/Message.cs
@@ -2,6 +2,7 @@
 {
     public class Message
     {
+        public string UserId { get; set; }
         public string UserFullName { get; set; }
         public string MessageText { get; set; }
     }
